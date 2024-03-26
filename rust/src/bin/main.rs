a8176c7d8918f7866ee4e9d3700bd77d38292685
Anton Yudin <dev@antonyudin.com>
2024-03-26 14:20:32 -0400

    added error handling

--- rust/src/bin/main.rs
+++ rust/src/bin/main.rs
@@ -4,7 +4,7 @@ use std::sync::mpsc;
 use std::sync::atomic::{AtomicI32, Ordering};
 use std::thread;
 
-use project::pool::Pool;
+use pool::pool::Pool;
 
 struct Value {
     value: i32
@@ -14,9 +14,9 @@ fn main() {
 
     let index = AtomicI32::new(0i32);
 
-    let pool = Pool::new(5, move || Value {
+    let pool = Pool::new(5, move || Ok(Value {
         value: { index.fetch_add(1, Ordering::Relaxed) }
-    });
+    }));
 
     let (tx, rx) = mpsc::channel();
 
@@ -26,15 +26,18 @@ fn main() {
 
         thread::spawn(move || {
             println!("thread [{}] started", i);
-            pool.process(move |resource| {
+            match pool.process(move |resource| {
                     println!("got resource [{}]", resource.get().value);
                     thread::sleep(Duration::from_secs(1));
                     if i == 1 {
                         println!("simulate an error by not writing to the channel");
                     } else if let Err(error) = tx.send(resource.get().value * 10) {
-                        println!("error: {}", error);
+                        println!("error sending data to a channel: {}", error);
                     }
-            });
+            }) {
+                Ok(_result) => println!("process [{}] finished successfully", i),
+                Err(error) => println!("process [{}] failed: {}", i, error)
+            }
         });
     }
 
@@ -43,6 +46,8 @@ fn main() {
     for value in rx {
         println!("received value {}", value);
     }
+
+    thread::sleep(Duration::from_secs(1));
 
 }
 
--- rust/src/pool.rs
+++ rust/src/pool.rs
@@ -1,5 +1,62 @@
 
-use std::sync::{Arc,Mutex};
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::fmt;
+
+
+#[derive(Debug)]
+pub enum PoolError {
+    AtCapacity(AtCapacityError),
+    Creator(CreatorError)
+}
+
+impl std::error::Error for PoolError {}
+
+impl fmt::Display for PoolError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PoolError::AtCapacity(error) => write!(f, "{}", error),
+            PoolError::Creator(error) => write!(f, "{}", error)
+        }
+    }
+}
+
+impl From<Box<dyn std::error::Error>> for PoolError {
+    fn from(error: Box<dyn std::error::Error>) -> PoolError {
+        PoolError::Creator(CreatorError { wrapped: error.to_string() })
+    }
+}
+
+
+
+#[derive(Debug, Clone)]
+pub struct AtCapacityError {
+    capacity: usize
+}
+
+impl fmt::Display for AtCapacityError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "pool at capacity: {}", self.capacity)
+    }
+}
+
+
+#[derive(Debug, Clone)]
+pub struct CreatorError {
+    wrapped: String
+}
+
+impl fmt::Display for CreatorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "creator error: {}", self.wrapped)
+    }
+}
+
+impl From<Box<dyn std::error::Error>> for CreatorError {
+    fn from(error: Box<dyn std::error::Error>) -> CreatorError {
+        CreatorError { wrapped: error.to_string() }
+    }
+}
 
 
 type OnDrop = Arc<dyn Fn() ->() + Send + Sync>;
@@ -21,67 +78,85 @@ impl <T> Resource<T> {
     pub fn get(&self) -> &T {
         &self.value
     }
+    pub fn get_as_mut(&mut self) -> &mut T {
+        &mut self.value
+    }
 }
 
 
-
-
-pub struct Pool<T, F: Fn() -> T> {
+pub struct Pool<T, F>
+where
+    F: Fn() -> Result<T, Box<dyn std::error::Error>>
+{
     resources: Arc<Mutex<Vec<Resource<T>>>>,
-    creator: Arc<F>
+    creator: Arc<F>,
+    capacity: usize,
+    size: Arc<AtomicUsize>
 }
 
-impl <T, F: Fn() -> T> Clone for Pool<T, F> {
+impl <T, F> Clone for Pool<T, F>
+where
+    F: Fn() -> Result<T, Box<dyn std::error::Error>>
+{
     fn clone(&self) -> Self {
         Pool::<T, F> {
             resources: self.resources.clone(),
-            creator: self.creator.clone()
+            creator: self.creator.clone(),
+            size: self.size.clone(),
+            capacity: self.capacity.clone()
         }
     }
 }
 
-impl <T, F: Fn() -> T> Pool<T, F> {
-    pub fn new(capacity: usize, creator: F) -> Pool<T, F>{
-        let mut resources: Vec<Resource<T>> = Vec::with_capacity(capacity);
-        for _i in 0..capacity {
-            resources.push(
-                Resource {
-                    value: (creator)(),
-                    on_drop: Arc::new(|| {
-                           println!("resource custom on_drop");
-                    })
-                }
-            );
-        }
+
+impl <T, F> Pool<T, F>
+where
+    F: Fn() -> Result<T, Box<dyn std::error::Error>>
+{
+    pub fn new(capacity: usize, creator: F) -> Pool<T, F> {
+        let resources: Vec<Resource<T>> = Vec::with_capacity(capacity);
         Pool {
             resources: Arc::new(Mutex::new(resources)),
-            creator: Arc::new(creator)
+            creator: Arc::new(creator),
+            capacity: capacity,
+            size: Arc::new(AtomicUsize::new(0))
         }
     }
 
-    pub fn process<R, FF: FnMut(&Resource<T>) -> R>(&mut self, mut callback: FF) -> Option<R> {
+    pub fn process<R, FF: FnMut(&Resource<T>) -> R>(&mut self, mut callback: FF) -> Result<R, PoolError> {
+        self.process_as_mut(|resource| callback(resource))
+    }
 
-        let resource: Option<Resource<T>>;
+    pub fn process_as_mut<R, FF: FnMut(&mut Resource<T>) -> R>(&mut self, mut callback: FF) -> Result<R, PoolError> {
+
+        let mut resource: Resource<T>;
 
         {
             let mut resources = self.resources.lock().unwrap();
-            resource = resources.pop();
-            println!("popped resource");
+            if resources.len() > 0 {
+                    resource = resources.pop().unwrap();
+                    println!("popped resource");
+            } else if self.capacity > self.size.load(Ordering::Relaxed) {
+                    resource = Resource {
+                        value: (self.creator)()?,
+                        on_drop: Arc::new(|| {
+                               println!("resource custom on_drop");
+                        })
+                    };
+                    self.size.fetch_add(1, Ordering::Relaxed);
+                    println!("created resource");
+            } else {
+                return Err(PoolError::AtCapacity(AtCapacityError {capacity: self.capacity}));
+            }
         }
 
-
-        match resource {
-            Some(resource) => {
-                let result = Some(callback(&resource));
-                {
-                    let mut resources = self.resources.lock().unwrap();
-                    resources.push(resource);
-                    println!("pushed resource");
-                }
-                result
-            },
-            None => None
+        let result = callback(&mut resource);
+        {
+            let mut resources = self.resources.lock().unwrap();
+            resources.push(resource);
+            println!("pushed resource");
         }
+        Ok(result)
     }
 
 }
